279c0b59e9a6b280473bf9b6d4c74a5179e61eca
emma <emma.pove24@gmail.com>
2021-12-12 12:51:19 -0300

    "cambio en la seccion de productos"

--- javascript/script.js
+++ javascript/script.js
@@ -1,97 +1,164 @@
-class Usuario {
-    constructor(nombre, usuario, clave) {
-        this.nombre = nombre;
-        this.usuario = usuario;
-        this.clave = clave;
-    }
-}
-const nombre = prompt("Ingrese su nombre");
-const usuario = prompt("Ingrese su usuario");
-const clave = prompt("Ingrese su clave");
-const nuevoUsuario = new Usuario(nombre, usuario, clave);
-alert(`Gracias ${nuevoUsuario.nombre} has creado el usuario xxxx y la clave xxxx correctamente. Apreta f12 para ver los datos creados`);
-console.log (`${nuevoUsuario.nombre} has creado el usuario ${nuevoUsuario.usuario} y la contraseña  
-${nuevoUsuario.clave} correctamente`)
+// class Usuario {
+//     constructor(nombre, usuario, clave) {
+//         this.nombre = nombre;
+//         this.usuario = usuario;
+//         this.clave = clave;
+//     }
+// }
+// const nombre = prompt("Ingrese su nombre");
+// const usuario = prompt("Ingrese su usuario");
+// const clave = prompt("Ingrese su clave");
+// const nuevoUsuario = new Usuario(nombre, usuario, clave);
+// alert(`Gracias ${nuevoUsuario.nombre} has creado el usuario xxxx y la clave xxxx correctamente. Apreta f12 para ver los datos creados`);
+// console.log (`${nuevoUsuario.nombre} has creado el usuario ${nuevoUsuario.usuario} y la contraseña  
+// ${nuevoUsuario.clave} correctamente`)
 
 
-function costos() {
-    if (producto1 == "1" && producto2 == "2") {
-        return juegoPc + juegoXbox;
-    } else if (producto1 == "1" && producto2 == "3") {
-        return juegoPc + juegoPlay;
-    } else if (producto1 == "2" && producto2 == "3") {
-        return juegoXbox + juegoPlay;
-    } else if (producto1 == "2" && producto2 == "1") {
-        return juegoXbox + juegoPc;
-    } else if (producto1 == "3" && producto2 == "1") {
-        return juegoXbox + juegoXbox;
-    } else if (producto1 == "3" && producto2 == "2") {
-        return juegoPlay + juegoXbox;
-    } else if (producto1 == "3" && producto2 == "3") {
-        return juegoPlay + juegoPlay;
-    }else if (producto1 == "1" && producto2 == "1") {
-        return juegoPc + juegoPc;
-    }else if (producto1 == "2" && producto2 == "2") {
-        return juegoXbox + juegoXbox;}
-    }
+// class articulos{
+//     constructor(producto,precio,cantidad){
+//         this.producto = producto;
+//         this.precio = precio;
+//         this.cantidad = cantidad;
 
-function totalF() {
-    if (producto1 == "1" && producto2 == "2") {
-        alert("elegiste juegoPc y juegoXbox");
-    } else if (producto1 == "1" && producto2 == "3") {
-        alert("elegiste juegoPc y JuegoPlay");
-    } else if (producto1 == "2" && producto2 == "1") {
-        alert("elegiste juegoXbox y juegoPc");
-    } else if (producto1 == "2" && producto2 == "3") {
-        alert("elegiste juegoXbox + juegoPc");
-    } else if (producto1 == "2" && producto2 == "2") {
-        alert("elegiste juegoXbox + juegoXbox");
-    } else if (producto1 == "3" && producto2 == "3") {
-        alert("elegiste juegoPc + juegoPc");
-    }else if (producto1 == "1" && producto2 == "1") {
-        alert("elegiste juegoPC + juegoPc");
-    } else if (producto1 == "3" && producto2 == "1") {
-        alert("elegiste JuegoPlay + juegoPc");
-    }else if (producto1 == "3" && producto2 == "2") {
-            alert("elegiste juegoPlay + juegoXbox");
-        }
-    }
-let juegoPc = 2500;
-let juegoXbox = 3400;
-let juegoPlay = 3000;
-let producto1 = prompt("Productos \n 1: juegoPc \n 2: juegoXbox \n 3: juegoPlay");
-let producto2 = prompt("Selecciona otro producto \n 1: juegoPc \n 2: juegoXbox \n 3: juegoPlay");
+//     }
+// }
 
-totalF();
-alert("Tu compra final mas el iva es de $" + costos() * 1.21);
+// const productos = [];
+// let products = prompt("¿Desea cargar un producto? (si/no)")
+// while(products === "si"){
+//     let producto = prompt("Seleccione un producto \ juegoPc, \ juegoXbox, \ juegoPlay");
+//     let precio = prompt("Seleccione el precio del producto 2500 \ 3000 \ 2700");
+//     let cantidad = prompt("Seleccione cuantos productos desea adquirir")
+//     let nuevoProducto = new articulos (producto , precio, cantidad);
+//     productos.push(nuevoProducto);
+//     products= prompt("¿Desea cargar un producto? (si/no)");
+//     console.log(productos)
+//     }
+//     if(products === "no"){
+//         alert("Gracias vuelva pronto")
+//     }
+// let total = 0;
+
+// for( i = 0; i < productos.length; i++){
+//     total = total + productos[i].precio * productos[i].cantidad;
+// }
+// alert("Su monto total es de $ " + total)
+
+// Incio de productos
+
+let arrayProducts = [
+    {
+        id: 1,
+        nombre: "COD",
+        precio: 3500,
+        imagen: "../assets/img/xbox1.png"
+    },
+    {
+        id: 2,
+        nombre: "WATCH DOGS",
+        precio: 3100,
+        imagen: "../assets/img/xbox2.jpg"
+    },
+    {
+        id: 3,
+        nombre: "HALO 4",
+        precio: 3000,
+        imagen: "../assets/img/xbox3.jpg"
+    },
+    {
+        id: 4,
+        nombre: "RALLY",
+        precio: 3000,
+        imagen: "../assets/img/xbox4.jpg"
+    },
+    {
+        id: 5,
+        nombre: "COD",
+        precio: 2500,
+        imagen: "../assets/img/xbox1.png"
+    },
+    {
+        id: 6,
+        nombre: "WATCH DOGS",
+        precio: 1500,
+        imagen: "../assets/img/xbox2.jpg"
+    },
+    {
+        id: 7,
+        nombre: "HALO 4",
+        precio: 4500,
+        imagen: "../assets/img/xbox3.jpg"
+    },
+    {
+        id: 8,
+        nombre: "RALLY",
+        precio: 4000,
+        imagen: "../assets/img/xbox4.jpg"
+    },
+    {
+        id: 9,
+        nombre: "COD",
+        precio: 2000,
+        imagen: "../assets/img/xbox1.png"
+    },
+    {
+        id: 10,
+        nombre: "WATCH DOGS",
+        precio: 3000,
+        imagen: "../assets/img/xbox2.jpg"
+    },
+    {
+        id: 11,
+        nombre: "HALO 4",
+        precio: 2500,
+        imagen: "../assets/img/xbox3.jpg"
+    },
+    {
+        id: 12,
+        nombre: "RALLY",
+        precio: 4500,
+        imagen: "../assets/img/xbox4.jpg"
+    },
+    {
+        id: 9,
+        nombre: "COD",
+        precio: 2000,
+        imagen: "../assets/img/xbox1.png"
+    },
+    {
+        id: 10,
+        nombre: "WATCH DOGS",
+        precio: 3000,
+        imagen: "../assets/img/xbox2.jpg"
+    },
+    {
+        id: 11,
+        nombre: "HALO 4",
+        precio: 2500,
+        imagen: "../assets/img/xbox3.jpg"
+    },
+    {
+        id: 12,
+        nombre: "RALLY",
+        precio: 4500,
+        imagen: "../assets/img/xbox4.jpg"
+    },
+
+];
+const carrito = document.getElementById("carrito")
+
+arrayProducts.forEach((elemento) => {
+    let card = document.createElement("div")
+    card.classList.add("card","col-sm-12", "col-lg-3")
+    card.innerHTML = `<img src="${elemento.imagen}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${elemento.nombre}</h5>
+      <p class="card-text">${elemento.precio}</p>
+      <a href="#" class="btn btn-primary">Agregar al carrito</a>
+    </div>`;
+
+    carrito.appendChild(card);
+});
 
 
-class articulos{
-    constructor(producto,precio,cantidad){
-        this.producto = producto;
-        this.precio = precio;
-        this.cantidad = cantidad;
 
-    }
-}
-
-const productos = [];
-let products = prompt("¿Desea cargar un producto? (si/no)")
-while(products === "si"){
-    let producto = prompt("Seleccione un producto \ juegoPc, \ juegoXbox, \ juegoPlay");
-    let precio = prompt("Seleccione el precio del producto 2500 \ 3000 \ 2700");
-    let cantidad = prompt("Seleccione cuantos productos desea adquirir")
-    let nuevoProducto = new articulos (producto , precio, cantidad);
-    productos.push(nuevoProducto);
-    products= prompt("¿Desea cargar un producto? (si/no)");
-    console.log(productos)
-    }
-    if(products === "no"){
-        alert("Gracias vuelva pronto")
-    }
-let total = 0;
-
-for( i = 0; i < productos.length; i++){
-    total = total + productos[i].precio * productos[i].cantidad;
-}
-alert("Su monto total es de $ " + total)
