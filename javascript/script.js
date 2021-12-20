832ef69b6f09f9842f577474ab1bba2aef710f0c
emma <emma.pove24@gmail.com>
2021-12-20 00:03:48 -0300

    "evento en mi javascript"

--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,5 @@
-class Usuario {
-    constructor(nombre, usuario, clave) {
-        this.nombre = nombre;
-        this.usuario = usuario;
-        this.clave = clave;
-    }
-}
-const nombre = prompt("Ingrese su nombre");
-const usuario = prompt("Ingrese su usuario");
-const clave = prompt("Ingrese su clave");
-const nuevoUsuario = new Usuario(nombre, usuario, clave);
-alert(`Gracias ${nuevoUsuario.nombre} has creado el usuario xxxx y la clave xxxx correctamente. Apreta f12 para ver los datos creados`);
-console.log (`${nuevoUsuario.nombre} has creado el usuario ${nuevoUsuario.usuario} y la contraseña  
-${nuevoUsuario.clave} correctamente`)
 
-
-
-
-// Incio de productos
-
-let arrayProducts = [
+let arrayProductos = [
     {
         id: 1,
         nombre: "COD",
@@ -117,20 +98,87 @@ let arrayProducts = [
     },
 
 ];
-const carrito = document.getElementById("carrito")
+const contenedor = document.getElementById("contenedor");
+contenedor.innerHTML = "";
 
-arrayProducts.forEach((elemento) => {
-    let card = document.createElement("div")
-    card.classList.add("card", "cardProducto","col-sm-12", "col-lg-3")
-    card.innerHTML = `<img src="${elemento.imagen}" class="card-img-top" alt="...">
+arrayProductos.forEach((producto, indice) => {
+  let card = document.createElement("div");
+  card.classList.add("card", "cards", "col-sm-12", "col-lg-3");
+  let html = `
+    <img src="${producto.imagen}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">${elemento.nombre}</h5>
-      <p class="card-text">${elemento.precio}</p>
-      <a href="#" class="btn btn-primary">Agregar al carrito</a>
-    </div>`;
-
-    carrito.appendChild(card);
+      <h5 class="card-title">${producto.nombre}</h5>
+      <p class="card-text">${producto.precio}</p>
+      <a href="#cart" class="btn btn-primary" onClick="agregarCarrito(${indice})">Comprar</a>
+    </div>
+      `;
+  card.innerHTML = html;
+  contenedor.appendChild(card);
 });
+
+const cart = [];
+
+const agregarCarrito = (indiceArray) => {
+  const indiceCarrito = cart.findIndex((elemento) => {
+    return elemento.id === arrayProductos[indiceArray].id;
+  });
+  if (indiceCarrito === -1) {
+    const productoAgregar = arrayProductos[indiceArray];
+    productoAgregar.cantidad = 1;
+    cart.push(productoAgregar);
+    compraCarrito();
+  } else {
+    cart[indiceCarrito].cantidad += 1;
+    compraCarrito();
+  }
+};
+
+let finCarrito = document.getElementById("cart");
+
+const compraCarrito = () => {
+  let total = 0;
+  finCarrito.className = "cart";
+  finCarrito.innerHTML = "";
+  if (cart.length > 0) {
+    cart.forEach((producto, indice) => {
+      total = total + producto.precio * producto.cantidad;
+      const carritoContainer = document.createElement("div");
+      carritoContainer.className = "producto-carrito";
+      carritoContainer.innerHTML = `
+        <img class="car-img" src="${producto.imagen}"/>
+        <div class="product-details">
+          ${producto.nombre}
+        </div>
+        <div class="product-details" > Cantidad: ${producto.cantidad}</div>
+        <div class="product-details"> Precio: $ ${producto.precio}</div>
+        <div class="product-details"> Subtotal: $ ${
+          producto.precio * producto.cantidad
+        }</div>
+        <button class="btn btn-danger"  id="remove-product" onClick="eliminarProducto(${indice})">Eliminar producto</button>
+         `;
+      finCarrito.appendChild(carritoContainer);
+    });
+    const totalContainer = document.createElement("div");
+    totalContainer.className = "total-carrito";
+    totalContainer.innerHTML = `<div class= "total"> TOTAL $ ${total}</div>
+    <button class= "btn btn-danger finalizar" id="finalizar" onClick="finalizarCompra()"> FINALIZAR COMPRA </button>`;
+    finCarrito.appendChild(totalContainer);
+  } else {
+    finCarrito.classList.remove("cart");
+  }
+};
+
+const eliminarProducto = (indice) => {
+  cart.splice(indice, 1);
+  compraCarrito();
+};
+const finalizarCompra = () => {
+  const total = document.getElementsByClassName("total")[0].innerHTML;
+  finCarrito.innerHTML = "";
+  const compraFinalizada = `<div><p class="total-compra"> Gracias por comprar, su ${total} </p></div>
+ `
+ finCarrito.innerHTML = compraFinalizada;
+};
 
 
 
